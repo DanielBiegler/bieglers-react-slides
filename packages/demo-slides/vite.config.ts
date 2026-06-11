32719585a6cc3a72278baebc5f81c52a39abe6d0
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2026-06-11 12:51:02 +0200

    add config for publishing to github pages

--- packages/demo-slides/vite.config.ts
+++ packages/demo-slides/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/bieglers-react-slides/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,4 +13,4 @@ export default defineConfig({
       ),
     },
   },
-})
+}))
